e30b8a574b61fd96236d64780e6428b499bc697d
dshriva <divya.shrivastava2014@gmail.com>
2018-01-18 02:26:01 -0600

    Added the condition to check whether switch, controller or none of the two.

--- src/main/java/com/bootstrap/Starter.java
+++ src/main/java/com/bootstrap/Starter.java
@@ -7,22 +7,31 @@ public class Starter {
 
     private static int port = 0;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         // Checking the arguments
-        if(!validateInput(args)) {
+        if (!validateInput(args)) {
             System.out.println("Terminating");
             return;
         }
 
-        System.out.println("The port number set for this machine is "+port);
+        System.out.println("The port number set for this machine is " + port);
 
     }
 
     private static boolean validateInput(String[] args) {
-        if(args != null && args.length == 1){
+        if (args != null && args.length == 2) {
             System.out.println("Bootstrapping your machine");
             System.out.println("Port Number entered is : " + args[0]);
+
+            if (args[1].equals("c")) {
+                System.out.println("It is a Controller");
+
+            } else if (args[1].equals("s")) {
+                System.out.println("It is a Switch");
+            } else {
+                System.out.println("Invalid argument. Enter a switch or a Controller.");
+            }
 
             try {
                 port = Integer.parseInt(args[0]);
