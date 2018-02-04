a601e5129ee3b2739bea2c6535d39f66173961cd
axred1 <35768412+axred1@users.noreply.github.com>
2018-02-04 11:16:51 -0500

    Minor updates to Path

--- src/main/java/com/domain/Path.java
+++ src/main/java/com/domain/Path.java
@@ -1,26 +1,27 @@
-package com.domain;
+package node;
 
 import java.util.HashSet;
 
-import static com.util.NetworkConstants.LINK;
-
 /*
  * created by divya at 1/28/2018
+ * updated by Ashley at 2/4/2018
  */
 public class Path {
     private String pathId;
+    private int node1;
+    private int node2;
     private int bandwidth;
     private int delay;
-    private boolean usable;
     private HashSet<String> vertexSet = new HashSet<String>(2);
 
-    public Path(int bandwidth, int delay, String id1, String id2,boolean usable) {
-        this.pathId = id1 + LINK + id2;
+    public Path(int bandwidth, int delay, String id1, String id2) {
+    	this.node1 = Integer.parseInt(id1);
+    	this.node2 = Integer.parseInt(id2);
+        this.pathId = id1 + "<->" + id2;
         this.bandwidth = bandwidth;
         this.delay = delay;
         vertexSet.add(id1);
         vertexSet.add(id2);
-        this.usable = usable;
     }
 
     public String getPathId() {
@@ -50,23 +51,12 @@ public class Path {
     public HashSet<String> getVertexSet() {
         return vertexSet;
     }
-
-    public boolean isUsable() {
-        return usable;
+    
+    public int getNode1() {
+        return node1;
     }
-
-    public void setUsable(boolean usable) {
-        this.usable = usable;
-    }
-
-    @Override
-    public String toString() {
-        return "Path{" +
-                "pathId='" + pathId + '\'' +
-                ", bandwidth=" + bandwidth +
-                ", delay=" + delay +
-                ", usable=" + usable +
-                '}';
+    
+    public int getNode2() {
+        return node2;
     }
 }
-
